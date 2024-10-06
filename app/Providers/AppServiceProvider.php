7a68723c8367942b047de44ddc02c0b8889fe527
victordiaz86 <victor@remoose.com>
2024-10-06 02:39:40 -0500

    Add users and roles, seeders and more

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Inertia::share([
+            'auth.user' => function () {
+                if (Auth::check()) {
+                    return [
+                        'id' => Auth::user()->id,
+                        'name' => Auth::user()->name,
+                        'email' => Auth::user()->email,
+                        'role' => Auth::user()->role ? Auth::user()->role->name : null,
+                    ];
+                }
+                return null;
+            },
+            // Compartir el idioma actual de la aplicación
+            'locale' => function () {
+                return App::getLocale();
+            },
+        ]);
     }
 }
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,12 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "../../../resources/images/logo2.jpeg";
+import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src={ApplicationLogo} width={120} height={120} />
                 </Link>
             </div>
 
